5deb686a26fa27721514957048882a83c621e97a
firzalay <firza7004@gmail.com>
2025-01-08 17:07:26 +0700

    feat: create dummy designs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="movie-container">
+                <h1>React Movie App</h1>
+                <div className="movie-search">
+                    <input type="text" placeholder="Search movie..." />
+                </div>
+                <div className="movie-wrapper">
+                    <div className="movie-content">
+                        <div className="movie-image">
+                            <img src="https://picsum.photos/720/1280" alt="" />
+                        </div>
+                        <div className="movie-title">
+                            <p>Helldrivers 2</p>
+                        </div>
+                        <div className="movie-date">
+                            <p>05 May 2017</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
